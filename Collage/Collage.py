05a3d985c3960de585f4025a166d37210389dac9
emre.karadag <emre.karadag@EMRE-KARADAG>
2023-11-08 17:05:53 +0300

    csv -fix

--- Collage/Collage.py
+++ Collage/Collage.py
@@ -1,20 +1,14 @@
-import pandas as pd
+﻿import pandas as pd
 import numpy as np
 import csv
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
-college2 = pd.read_csv('college1.csv', index_col=0)
-college3 = college.rename({'Unnamed: 0': 'College'}, axis=1)
-college3 = college3.set_index('College')
 
-
-
-print(college2)
-print(college2.describe())
+college = pd.read_csv("College.csv", index_col=0)
+print(college.columns)
 print("--------------------------------")
-print(college3)
-print(college3.describe())
+print(college.describe())
 
-
+print(college)
