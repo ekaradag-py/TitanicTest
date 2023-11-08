00e3ae0ad10268b9691a7fea83ad1d1b22576a2d
emre.karadag <emre.karadag@EMRE-KARADAG>
2023-11-08 15:21:36 +0300

    rename colon

--- Collage/Collage.py
+++ Collage/Collage.py
@@ -5,10 +5,16 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
-data = pd.read_csv("Collage.csv")
+college2 = pd.read_csv('college1.csv', index_col=0)
+college3 = college.rename({'Unnamed: 0': 'College'}, axis=1)
+college3 = college3.set_index('College')
 
 
-print(data)
-print(data.describe())
+
+print(college2)
+print(college2.describe())
+print("--------------------------------")
+print(college3)
+print(college3.describe())
 
 
